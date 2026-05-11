abe25e51b437aa59aec76479e7cfb9b60fd2fd6f
Swapnil Sarker <swapnilsarker3112@gmail.com>
2026-05-11 13:17:56 +0600

    Refactor imports and reformat route definitions

--- backend/routes/watched.js
+++ backend/routes/watched.js
@@ -1,11 +1,23 @@
 const express = require('express');
+
 const router = express.Router();
-const { toggleWatched, getMovieWatched } = require('../controllers/watchedController');
-const { protect } = require('../middleware/auth');
+
+const {
+  toggleWatched,
+  getMovieWatched,
+} = require('../controllers/watchedController');
+
+const {
+  protect,
+} = require('../middleware/auth');
 
 router.use(protect);
 
 router.post('/toggle', toggleWatched);
-router.get('/movie/:movieId/group/:groupId', getMovieWatched);
+
+router.get(
+  '/movie/:movieId/group/:groupId',
+  getMovieWatched
+);
 
 module.exports = router;
